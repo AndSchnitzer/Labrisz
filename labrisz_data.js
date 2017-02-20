e268983451c0195cf090bf60d68bafa54f7e140e
AndSchnitzer <andschnitzer@gmail.com>
2017-02-20 11:44:42 +0100

    sources added to the rest of the entries.

--- labrisz_data.js
+++ labrisz_data.js
@@ -66,7 +66,7 @@ var LABRISZ_DATA=[
       "lng": 19.0639768
     },
     "name": "Di\u00f3fa Restaurant",
-    "photo": "This restaurant was monitored by secret police. The secret police do not share their records, so their photos remain unsearchable.",
+    "photo": "This restaurant was monitored by secret police from the 1970s. The secret police do not share their records, so their photos remain unsearchable.",
     "photos": [
       {
         "text": "Menu cover from 1974 showing a collection of other LGBT spaces behind it. (Image from H\u00e1tt\u00e9r Society Archives)",
@@ -81,7 +81,7 @@ var LABRISZ_DATA=[
         "img": "diofa_restaurant_2.jpg"
       },
       {
-        "text": "Another view of the restaurant and gay bar. (Image from Kristina Cajkovicova)",
+        "text": "Another view of the restaurant and gay bar in 2016. (Image from Kristina Cajkovicova)",
         "img": "diofa_restaurant_3.jpg"
       }
     ],
@@ -89,7 +89,7 @@ var LABRISZ_DATA=[
   },
   {
     "address": "Budapest VIII 1088, Szentkir\u00e1lyi utca 8",
-    "description": "1991 bar with women-only nights",
+    "description": "Bar with women-only nights in 1991. Budapest VIII 1088, Szentkir\u00e1lyi utca 8. No photo yet.",
     "location": {
       "lat": 47.493973,
       "lng": 19.0627571
@@ -106,24 +106,24 @@ var LABRISZ_DATA=[
   },
   {
     "address": "Budapest 1068, Kir\u00e1ly utca 56",
-    "description": "Labrisz office location from 2006 to 2008",
+    "description": "Labrisz office location from 2006 to 2008. Budapest 1068, Kir\u00e1ly utca 56",
     "location": {
       "lat": 47.5025783,
       "lng": 19.0615072
     },
     "name": "Labrisz",
-    "photo": "This was an early location for Labrisz, but it has relocated. Photo taken in 2016.",
+    "photo": "This was an early location for Labrisz, but it has relocated. Photo taken in 2016. (Image from Kristina Cajkovicova)",
     "photos": [
       {
-        "text": "This was an early location for Labrisz, but it has relocated. Photo taken in 2016.",
+        "text": "This was an early location for Labrisz, but it has relocated. Photo taken in 2016. (Image from Kristina Cajkovicova)",
         "img": "labrisz_kiraly_1.jpg"
       },
       {
-        "text": "This was an early location for Labrisz, but it has relocated. Photo taken in 2016.",
+        "text": "This was an early location for Labrisz, but it has relocated. Photo taken in 2016. (Image from Kristina Cajkovicova)",
         "img": "labrisz_kiraly_2.jpg"
       },
       {
-        "text": "This was an early location for Labrisz, but it has relocated. Photo taken in 2016.",
+        "text": "This was an early location for Labrisz, but it has relocated. Photo taken in 2016. (Image from Kristina Cajkovicova)",
         "img": "labrisz_kiraly_3.jpg"
       }
     ],
@@ -131,7 +131,7 @@ var LABRISZ_DATA=[
   },
   {
     "address": "Heged\u0171s Gyula utca, 13. Ker\u00fcle",
-    "description": "1980s theater",
+    "description": "Theater. Heged\u0171s Gyula utca, 13. Ker\u00fcle",
     "location": {
       "lat": 47.519328,
       "lng": 19.0549112
@@ -140,7 +140,7 @@ var LABRISZ_DATA=[
     "photo": "Old historic photograph of Ipoly Cinema",
     "photos": [
       {
-        "text": "In the 1980s (1987), it was rented and operated by Ildi Juh\u00e1sz, who held private lesbian parties after the film screenings before the change of r\u00e9gime. It ran secretly.",
+        "text": "In the 1980s (1987), it was rented and operated by Ildi Juh\u00e1sz, who held private lesbian parties after the film screenings before the change of r\u00e9gime. It ran secretly. (Image from Fortepan online archives)",
         "img": "ipoly.jpg"
       }
     ],
@@ -148,7 +148,7 @@ var LABRISZ_DATA=[
   },
   {
     "address": "Budapest VI, Izabella utca 85",
-    "description": "If Only Bar opened in 2005 by two women",
+    "description": "If Only Bar opened in 2005 by two women. Budapest VI, Izabella utca 85",
     "location": {
       "lat": 47.5106985,
       "lng": 19.0612149
@@ -157,11 +157,11 @@ var LABRISZ_DATA=[
     "photo": "This bar has changed names. These are the 2016 photos.",
     "photos": [
       {
-        "text": "A women-only bar opened in 2005 by two women.",
+        "text": "A women-only bar opened in 2005 by two women. (Image from Kristina Cajkovicova)",
         "img": "barcsak_pub_1.jpg"
       },
       {
-        "text": "2016 location.",
+        "text": "2016 location. (Image from Kristina Cajkovicova)",
         "img": "barcsak_pub_2.jpg"
       }
     ],
@@ -169,7 +169,7 @@ var LABRISZ_DATA=[
   },
   {
     "address": "Budapest VIII, Vas utca 2",
-    "description": "One of the oldest \"gay\" restaurants that is still open",
+    "description": "One of the oldest \"gay\" restaurants that is still open. Budapest VIII, Vas utca 2",
     "location": {
       "lat": 47.4950799,
       "lng": 19.0639394
@@ -178,7 +178,7 @@ var LABRISZ_DATA=[
     "photo": "",
     "photos": [
       {
-        "text": "photo from the website",
+        "text": "Photo from the restaurant website.",
         "img": "club_93_pizzeria.jpg"
       }
     ],
@@ -186,7 +186,7 @@ var LABRISZ_DATA=[
   },
   {
     "address": "Budapest VII, Kazinczy utca 52\/c",
-    "description": "Lesbian, gay, mixed caf\u00e9 in the heart of Budapest.",
+    "description": "Lesbian, gay, mixed caf\u00e9 at Budapest VII, Kazinczy utca 52\/c. Needs a photo.",
     "location": {
       "lat": 47.4993308,
       "lng": 19.058418
@@ -203,24 +203,24 @@ var LABRISZ_DATA=[
   },
   {
     "address": "Semmelweis utca 21 1051",
-    "description": "Nagy Mez\u00f3 utca A lesbian-friendly bar that gave place for the first regular lesbian parties from the late 1990s",
+    "description": "A lesbian-friendly bar that gave place for the first regular lesbian parties from the late 1990s. Semmelweis utca 21 1051",
     "location": {
       "lat": 47.4954933,
       "lng": 19.0552723
     },
     "name": "Cafe Eklektika",
-    "photo": "1st: exterior \/ on fb page of eklektika",
+    "photo": "1st: exterior \/ on facebook page of Eklektika",
     "photos": [
       {
-        "text": "A lesbian-friendly bar that gave place for the first regular lesbian parties from the late 1990s and the first same-sex ball-dancing classes from 2000. It was a place for women to have a beer and talk together, and later to dance, but it was hard for businesses to stay open if they only catered to female customers. It moved and changed its profile in 2006. Now it is closed.",
+        "text": "A lesbian-friendly bar that gave place for the first regular lesbian parties from the late 1990s and the first same-sex ball-dancing classes from 2000. It was a place for women to have a beer and talk together, and later to dance, but it was hard for businesses to stay open if they only catered to female customers. It moved and changed its profile in 2006. (Image from Eklektika Facebook page)",
         "img": "cafeeklektika.jpg"
       },
       {
-        "text": "A lesbian-friendly bar that gave place for the first regular lesbian parties from the late 1990s and the first same-sex ball-dancing classes from 2000. It was a place for women to have a beer and talk together, and later to dance, but it was hard for businesses to stay open if they only catered to female customers. It moved and changed its profile in 2006. Now it is closed.",
+        "text": "(Image from Eklektika Facebook page)",
         "img": "cafeeklektika_2.jpg"
       },
       {
-        "text": "A lesbian-friendly bar that gave place for the first regular lesbian parties from the late 1990s and the first same-sex ball-dancing classes from 2000. It was a place for women to have a beer and talk together, and later to dance, but it was hard for businesses to stay open if they only catered to female customers. It moved and changed its profile in 2006. Now it is closed.",
+        "text": "(Image from Eklektika Facebook page)",
         "img": "cafeeklektika_3.jpg"
       }
     ],
@@ -228,7 +228,7 @@ var LABRISZ_DATA=[
   },
   {
     "address": "Budapest VIII R\u00e1k\u00f3czi utca 51",
-    "description": "Angel Bar location 3 from 1993",
+    "description": "Angel Bar location 3 from 1993. Budapest VIII R\u00e1k\u00f3czi utca 51",
     "location": {
       "lat": 47.49762,
       "lng": 19.0708663
@@ -237,19 +237,19 @@ var LABRISZ_DATA=[
     "photo": "old location",
     "photos": [
       {
-        "text": "Had women-only nights. This was the first place for guys and girls to gather in an obvious setting. One of the first and most well-known gay bars in Budapest, existing between 1991 and 2006, changing its location five times. Photo from 2016.",
+        "text": "Hosted women-only nights. This was the first place for guys and girls to gather in an obvious setting. One of the first and most well-known gay bars in Budapest, existing between 1991 and 2006, changing its location five times. Photo from 2016.",
         "img": "angyal_bar_2_1.jpg"
       },
       {
-        "text": "This is the old location. It is no longer here. Photo from 2016.",
+        "text": "It is no longer here. 2016. (Image from Kristina Cajkovicova)",
         "img": "angyal_bar_2_2.jpg"
       },
       {
-        "text": "This is the old location. It is no longer here. Photo from 2016.",
+        "text": "Former location. 2016. (Image from Kristina Cajkovicova)",
         "img": "angyal_bar_2_3.jpg"
       },
       {
-        "text": "This is the old location. It is no longer here. Photo from 2016.",
+        "text": "Former location. 2016. (Image from Kristina Cajkovicova)",
         "img": "angyal_bar_2_4.jpg"
       }
     ],
@@ -257,7 +257,7 @@ var LABRISZ_DATA=[
   },
   {
     "address": "Budapest VII Szovets\u00e9g utca 33",
-    "description": "Fourth Angel Bar location from 1995 to 1997",
+    "description": "Fourth Angel Bar location from 1995 to 1997. Budapest VII Szovets\u00e9g utca 33",
     "location": {
       "lat": 47.5015491,
       "lng": 19.0709257
@@ -266,19 +266,19 @@ var LABRISZ_DATA=[
     "photo": "photo from 2016",
     "photos": [
       {
-        "text": "This used to be the location of the fourth Angel Bar. It had women-only nights. This was the first place for guys and girls to gather in an obvious setting. One of the first and most well-known gay bars in Budapest, existing between 1991 and 2006, changing its location five times.",
+        "text": "This used to be the location of the fourth Angel Bar. It had women-only nights. This was the first place for guys and girls to gather in an obvious setting. One of the first and most well-known gay bars in Budapest, existing between 1991 and 2006, changing its location five times. It is no longer here. 2016. (Image from Kristina Cajkovicova)",
         "img": "angyal_3_1.jpg"
       },
       {
-        "text": "This used to be the location of the fourth Angel Bar. It had women-only nights. This was the first place for guys and girls to gather in an obvious setting. One of the first and most well-known gay bars in Budapest, existing between 1991 and 2006, changing its location five times.",
+        "text": "This used to be the location of the fourth Angel Bar. It had women-only nights. This was the first place for guys and girls to gather in an obvious setting. One of the first and most well-known gay bars in Budapest, existing between 1991 and 2006, changing its location five times. It is no longer here. 2016. (Image from Kristina Cajkovicova)",
         "img": "angyal_3_2.jpg"
       }
     ],
     "year": "1995-1997"
   },
   {
-    "address": "Budapest 1132 Visegr\u00e1di utca 10 ",
-    "description": "Early H\u00e1tt\u00e9r Society location opened in 1995",
+    "address": "Budapest 1132 Visegr\u00e1di utca 10",
+    "description": "Early H\u00e1tt\u00e9r Society location opened in 1995. Budapest 1132 Visegr\u00e1di utca 10",
     "location": {
       "lat": 47.5124947,
       "lng": 19.0517776
@@ -287,7 +287,7 @@ var LABRISZ_DATA=[
     "photo": "Google Maps photo",
     "photos": [
       {
-        "text": "This image was pulled from Google Maps",
+        "text": "(Image from Google Maps)",
         "img": "hatter_visegradi.jpg"
       }
     ],
@@ -295,7 +295,7 @@ var LABRISZ_DATA=[
   },
   {
     "address": "Budapest 1554 Csan\u00e1dy utca 4\/b",
-    "description": "Current H\u00e1tt\u00e9r Society location",
+    "description": "Current H\u00e1tt\u00e9r Society location (photo needed). Budapest 1554 Csan\u00e1dy utca 4\/b",
     "location": {
       "lat": 47.5159632,
       "lng": 19.0560498
@@ -312,7 +312,7 @@ var LABRISZ_DATA=[
   },
   {
     "address": "Budapest 1072, Klauz\u00e1l t\u00e9r 5",
-    "description": "Bar and party location",
+    "description": "Bar and party location. Budapest 1072, Klauz\u00e1l t\u00e9r 5",
     "location": {
       "lat": 47.498996,
       "lng": 19.0617946
@@ -321,7 +321,7 @@ var LABRISZ_DATA=[
     "photo": "exterior shot",
     "photos": [
       {
-        "text": "This was a bar and party location.",
+        "text": "This was a bar and party location. (Image from Eifert J\u00E1nos",
         "img": "2012.10.14-pepita-ofelia-bar-a-klauzal-teren.jpg"
       }
     ],
@@ -329,7 +329,7 @@ var LABRISZ_DATA=[
   },
   {
     "address": "Budapest 1085, M\u00e1ria utca 54",
-    "description": "An alternative non-profit community radio channel from 2009 to present. Broadcast: Z\u00e1rtkoru L\u00e1nyok (Exclusive girls)",
+    "description": "An alternative non-profit community radio channel from 2009 to present. The broadcast was named Z\u00e1rtkoru L\u00e1nyok (Exclusive girls) Budapest 1085, M\u00e1ria utca 54",
     "location": {
       "lat": 47.487185,
       "lng": 19.066306
@@ -338,7 +338,7 @@ var LABRISZ_DATA=[
     "photo": "",
     "photos": [
       {
-        "text": "photo from 2016 which does not appear to have a sign for the radio station",
+        "text": "Exterior building photo (Image from Andrea Schnitzer)",
         "img": "Tilos.jpg"
       }
     ],
@@ -346,7 +346,7 @@ var LABRISZ_DATA=[
   },
   {
     "address": "Budapest 1066, Zichy Jeno utca 29",
-    "description": "1st Labrisz evening location in 1994",
+    "description": "The 1st Labrisz evening location was here in 1994 at Budapest 1066, Zichy Jeno utca 29. Now a bar.",
     "location": {
       "lat": 47.5047571,
       "lng": 19.0558435
@@ -355,15 +355,15 @@ var LABRISZ_DATA=[
     "photo": "photo from Nov 23rd 2016",
     "photos": [
       {
-        "text": "The first Labrisz meeting was held in a small apartment. It started in an \u00d3vegylet back room. The \u00d3vegylet Foundation was the first AIDS prevention organisation, existing between 1994 and 2001.",
+        "text": "The first Labrisz meeting was held in a small apartment. It started in an \u00d3vegylet back room. The \u00d3vegylet Foundation was the first AIDS prevention organisation, existing between 1994 and 2001. (Image from Kristina Cajkovicova (2016))",
         "img": "ovegyet_foundation_1.jpg"
       },
       {
-        "text": "The first Labrisz meeting was held in a small apartment. It started in an \u00d3vegylet back room. The \u00d3vegylet Foundation was the first AIDS prevention organisation, existing between 1994 and 2001.",
+        "text": "The first Labrisz meeting was held in a small apartment. It started in an \u00d3vegylet back room. The \u00d3vegylet Foundation was the first AIDS prevention organisation, existing between 1994 and 2001. (Image from Kristina Cajkovicova (2016))",
         "img": "ovegyet_foundation_2.jpg"
       },
       {
-        "text": "The first Labrisz meeting was held in a small apartment. It started in an \u00d3vegylet back room. The \u00d3vegylet Foundation was the first AIDS prevention organisation, existing between 1994 and 2001..",
+        "text": "The first Labrisz meeting was held in a small apartment. It started in an \u00d3vegylet back room. The \u00d3vegylet Foundation was the first AIDS prevention organisation, existing between 1994 and 2001. (Image from Kristina Cajkovicova (2016))",
         "img": "ovegyet_foundation_3.jpg"
       }
     ],
@@ -371,24 +371,24 @@ var LABRISZ_DATA=[
   },
   {
     "address": "Budapest V. Belgr\u00e1d rkp. 23",
-    "description": "A club with a mixed crowd from 1995 to present",
+    "description": "A club with a mixed male/female crowd from 1995 to present. Budapest V. Belgr\u00e1d rkp. 23",
     "location": {
       "lat": 47.4900714,
       "lng": 19.0506359
     },
     "name": "Capella Caff\u00e9",
-    "photo": "photo from Nov 23rd 2016. Still there.",
+    "photo": "Still open.",
     "photos": [
       {
-        "text": "One of the oldest gay bars in Budapest with a mixed-gender crowd.",
+        "text": "One of the oldest gay bars in Budapest with a mixed-gender crowd. Still open. (Image from Kristina Cajkovicova (2016))",
         "img": "capella_1.jpg"
       },
       {
-        "text": "One of the oldest gay bars in Budapest with a mixed-gender crowd.",
+        "text": "One of the oldest gay bars in Budapest with a mixed-gender crowd. (Image from Kristina Cajkovicova (2016))",
         "img": "capella_2.jpg"
       },
       {
-        "text": "One of the oldest gay bars in Budapest with a mixed-gender crowd.",
+        "text": "One of the oldest gay bars in Budapest with a mixed-gender crowd. (Image from Kristina Cajkovicova (2016))",
         "img": "capella_3.jpg"
       }
     ],
@@ -396,7 +396,7 @@ var LABRISZ_DATA=[
   },
   {
     "address": "1052 Ferencziek tere 1",
-    "description": "In the evenings in the 1970s and 80s this turned into a gay venue. 95 percent of the crowd here were men. Secret Police raids were conducted here to scare the gay community.",
+    "description": "In the evenings in the 1970s and 80s this turned into a gay venue. 95 percent of the crowd here were men. Secret Police raids were conducted here to instill fear in the gay community. 1052 Ferencziek tere 1",
     "location": {
       "lat": 47.4933192,
       "lng": 19.0533043
@@ -405,35 +405,35 @@ var LABRISZ_DATA=[
     "photo": "photos from Hatter",
     "photos": [
       {
-        "text": "",
+        "text": "(Image from H\u00e1tt\u00e9r Society Archives)",
         "img": "egyetem_exterior_1.jpg"
       },
       {
-        "text": "",
+        "text": "(Image from H\u00e1tt\u00e9r Society Archives)",
         "img": "egyetem_exterior_2.jpg"
       },
       {
-        "text": "",
+        "text": "(Image from H\u00e1tt\u00e9r Society Archives)",
         "img": "egyetem_exterior_3.jpg"
       },
       {
-        "text": "",
+        "text": "(Image from H\u00e1tt\u00e9r Society Archives)",
         "img": "egyetem_belso.jpg"
       },
       {
-        "text": "",
+        "text": "(Image from H\u00e1tt\u00e9r Society Archives)",
         "img": "egyetem_menu_1.jpg"
       },
       {
-        "text": "",
+        "text": "(Image from H\u00e1tt\u00e9r Society Archives)",
         "img": "egyetem_menu_2.jpg"
       },
       {
-        "text": "",
+        "text": "(Image from H\u00e1tt\u00e9r Society Archives)",
         "img": "egyetem_menu_3.jpg"
       },
       {
-        "text": "",
+        "text": "(Image from H\u00e1tt\u00e9r Society Archives)",
         "img": "egyetem_menu_4.jpg"
       }
     ],
